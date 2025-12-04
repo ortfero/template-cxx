9ec2cc409457a616a246f59451010d4db49e2e2e
ortfero <ortfero@gmail.com>
2025-12-04 17:34:21 +0200

    update

---
+++ bench/bench.cpp
@@ -0,0 +1,8 @@
+#define ANKERL_NANOBENCH_IMPLEMENT
+#include <nanobench.h>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+#define TEMPLATE_CXX_ENABLE_BENCH
+
---
+++ test/test.cpp
@@ -0,0 +1,4 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+#define TEMPLATE_CXX_ENABLE_TEST
